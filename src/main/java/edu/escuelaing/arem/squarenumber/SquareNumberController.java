8cfdcd94cc8dc931965e976df2979bfc8c41f87f
Alejandro Anzola <anzola95avila@gmail.com>
2018-04-03 20:13:39 -0500

    agrega redireccion de peticion get de numero a servidor de heroku y retorna el numero de respuesta del servidor

--- src/main/java/edu/escuelaing/arem/squarenumber/SquareNumberController.java
+++ src/main/java/edu/escuelaing/arem/squarenumber/SquareNumberController.java
@@ -1,12 +1,12 @@
-/*
- */
 package edu.escuelaing.arem.squarenumber;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -16,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/square")
 public class SquareNumberController {
 
+    public static final String SERVER_ADDRESS = "https://arem-proyecto2.herokuapp.com/";
+    public static final String SQUARE_GET_PARAM = "square";
+    
     @GetMapping
     public @ResponseBody
-    String squareNumber(@RequestParam("value") int value) {
-        return "" + (value * value);
+    String squareNumber(@RequestParam("value") long value) {
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response = template.getForEntity(SERVER_ADDRESS + SQUARE_GET_PARAM + "?value=" + value, String.class);
+        
+        return response.getBody();
     }
 }
